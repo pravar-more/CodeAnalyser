7b87cf0f983aeced0ec41017de3767a750e42693
pravar-more <more.pravar@gmail.com>
2025-02-20 20:38:12 +0530

    new

--- codeAnalyser/shellCommands/commands.sh
+++ codeAnalyser/shellCommands/commands.sh
@@ -3,9 +3,13 @@ python -m venv venv
 .\venv\Scripts\activate         #for windows
 source venv/bin/activate        #for linux
 
-
+#cd $HOME/codeAnalyser/docs/
+cd ./codeAnalyser/docs/
 pip install -r requirements.txt
 
+cd ./codeAnalyser
 python setup.py install
 
+cd ./codeAnalyser/streamlit_app
+streamlit run .\app.py
 deactivate
