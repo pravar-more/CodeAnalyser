9b048ae5a77cde2c759d26731e11758178f0b583
pravar-more <more.pravar@gmail.com>
2025-02-20 17:49:01 +0530

    add

--- codeAnalyser/streamlit_app/pages/page1.py
+++ codeAnalyser/streamlit_app/pages/page1.py
@@ -1,9 +1,33 @@
 # streamlit_app/pages/page1.py
 import streamlit as st
-from streamlit_app.components.component1 import show_repo_info
+# from streamlit_app.components.component1 import show_repo_info
 
-def page1():
-    st.title("Page 1 - Repository Information")
-    repo_url = st.text_input("Enter Git Repository URL for Page 1")
-    if repo_url:
-        show_repo_info(repo_url)
+
+def page1():  
+    st.title("Git Repository Analysis")
+    repo_url = st.text_input("Enter Git Repository URL")
+    if st.button("Analyze"):
+        if repo_url:
+            st.write("hello world")
+        else:
+            st.write("Please enter a valid Git repository URL.")    
+
+    else:
+        st.write("hello not available")
+
+
+
+
+
+
+
+
+
+
+# def page1():
+#     st.title("Page 1 - Repository Information")
+#     repo_url = st.text_input("Enter Git Repository URL for Page 1")
+#     if repo_url:
+#         show_repo_info(repo_url)
+#     else:
+#         st.write("hello not available")
